932dcec199775f0bd253cc42264a9a50e5e31f66
nikki93 <s.nikhilesh@gmail.com>
2012-01-31 00:02:01 -0500

    Make wait() wait in blocks to allow cancelling execution

--- userspace.py
+++ userspace.py
@@ -6,9 +6,23 @@
 
 import time
 import vec2d
+import pygame
 
 space = {}
 
 space['Vec2d'] = vec2d.Vec2d
-space['wait'] = time.sleep
+
+# sleep in blocks of 'wait_block' to allow killing of thread
+wait_block = 1
+def wait(t):
+    loops = t // wait_block
+    extra = t - float(loops * wait_block)
+    print loops, extra
+
+    while loops > 0:
+        loops -= 1
+        time.sleep(wait_block)
+    if (extra):
+        time.sleep(extra)
+space['wait'] = wait
 

811e331a276b92c03ecc2b57d492bdfcbe0995b6
nikki93 <s.nikhilesh@gmail.com>
2012-01-31 00:36:12 -0500

    Add tabs, kill runner properly exiting

--- gui.py
+++ gui.py
@@ -49,13 +49,14 @@ class KThread(threading.Thread):
         self.killed = True
 
 class Runner(KThread):
-    def __init__(self, text):
+    def __init__(self, filename, text):
         super(Runner, self).__init__()
         self.text = text
+        self.filename = filename
 
     def run(self):
         try:
-            code = compile(self.text, "code", "exec")
+            code = compile(self.text, self.filename, "exec")
             exec(code, userspace.space, userspace.space)
         except Exception as e:
             gtk.threads_enter()
@@ -78,50 +79,61 @@ def do_quit(*args):
     shared.stop_game = True
 
 entries = [
-  ("FileMenu", None, "_File"),     # name, stock id, label
-  ("EditMenu", None, "_Edit"),       # name, stock id, label
-  ("RunMenu", None, "_Run"),       # name, stock id, label
-  ("HelpMenu", None, "_Help"),     # name, stock id, label
+  ("FileMenu", None, "_File"),
+  ("EditMenu", None, "_Edit"),
+  ("RunMenu", None, "_Run"),
+  ("HelpMenu", None, "_Help"),
 
-#  ("New", gtk.STOCK_NEW,            # name, stock id
-#    "_New", "<control>N",           # label, accelerator
-#    "Create a new file",            # tooltip
-#    do_new),      
-#  ("Open", gtk.STOCK_OPEN,          # name, stock id
-#    "_Open","<control>O",           # label, accelerator
-#    "Open a file",                  # tooltip
+#  ("Open", gtk.STOCK_OPEN,
+#    "_Open","<control>O",
+#    "Open a file",
 #    do_open), 
-#  ("Save", gtk.STOCK_SAVE,          # name, stock id
-#    "_Save","<control>S",           # label, accelerator
-#    "Save current file",            # tooltip
+#  ("Save", gtk.STOCK_SAVE,
+#    "_Save","<control>S",
+#    "Save current file",
 #    do_save),
-#  ("SaveAs", gtk.STOCK_SAVE,        # name, stock id
-#    "Save _As...", None,            # label, accelerator
-#    "Save to a file",               # tooltip
+#  ("SaveAs", gtk.STOCK_SAVE,
+#    "Save _As...", None,
+#    "Save to a file",
 #    do_save_as),
-  ("Quit", gtk.STOCK_QUIT,          # name, stock id
-    "_Quit", "Escape",              # label, accelerator
-    "Quit",                         # tooltip
+  ("NewTab", gtk.STOCK_ADD,
+    "_New Tab", "<Control>N",
+    "Make a new tab",
+    lambda *args: shared.gui.add_page()), 
+  ("CloseTab", gtk.STOCK_CLOSE,
+    "_Close Tab", "<Control>W",
+    "Close currently visible tab",
+    lambda *args: shared.gui.close_page()), 
+  ("NextTab", gtk.STOCK_GO_FORWARD,
+    "_Next Tab", "<Control>Right",
+    "Switch to next tab",
+    lambda *args: shared.gui.next_page()), 
+  ("PrevTab", gtk.STOCK_GO_BACK,
+    "_Previous Tab", "<Control>Left",
+    "Switch to previous tab",
+    lambda *args: shared.gui.prev_page()), 
+  ("Quit", gtk.STOCK_QUIT,
+    "_Quit", "Escape",
+    "Exit the program",
     do_quit), 
 
-  ("Run", gtk.STOCK_MEDIA_PLAY,        # name, stock id
-    "_Run", "<control>R",           # label, accelerator
-    "Run",                          # tooltip
-    lambda *args: shared.gui.run()), 
-
-  ("Cancel", gtk.STOCK_MEDIA_STOP,        # name, stock id
-    "_Cancel", "<control>E",           # label, accelerator
-    "Cancel",                          # tooltip
-    lambda *args: shared.gui.cancel()), 
-
-  ("Undo", gtk.STOCK_UNDO,        # name, stock id
-    "_Undo", "<control>Z",           # label, accelerator
-    "Undo",                          # tooltip
+  ("Undo", gtk.STOCK_UNDO,
+    "_Undo", "<control>Z",
+    "Undo",
     lambda *args: shared.gui.undo()), 
 
-  ("About", None,                   # name, stock id
-    "_About", "<control>A",         # label, accelerator
-    "About",                        # tooltip
+  ("Run", gtk.STOCK_MEDIA_PLAY,
+    "_Run", "<control>R",
+    "Run code in current tab",
+    lambda *args: shared.gui.run()), 
+  ("Cancel", gtk.STOCK_MEDIA_STOP,
+    "_Cancel", "<control>E",
+    "Cancel",
+    lambda *args: shared.gui.cancel()), 
+
+  ("About", None,
+    "_About", "<control>A",
+    "About",
     do_about),
 ]
 
@@ -129,6 +141,12 @@ ui_info ="""
 <ui>
   <menubar name='MenuBar'>
     <menu action='FileMenu'>
+      <menuitem action='NewTab'/>
+      <menuitem action='CloseTab'/>
+      <separator/>
+      <menuitem action='NextTab'/>
+      <menuitem action='PrevTab'/>
+      <separator/>
       <menuitem action='Quit'/>
     </menu>
 
@@ -148,16 +166,54 @@ ui_info ="""
 </ui>
 """
 
+codelang = gtksourceview.language_manager_get_default().guess_language("test.py")
+codefont = pango.FontDescription('monospace')
+
+class Page:
+    def __init__(self, gui, label):
+        self.label = label
+        self.gui = gui
+
+        # text edit box inside a scrolled window
+        self.buf = gtksourceview.Buffer()
+        self.text = gtksourceview.View(self.buf)
+        self.sw = gtk.ScrolledWindow()
+        self.sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.sw.set_shadow_type(gtk.SHADOW_IN)
+        self.sw.add(self.text)
+
+        self.buf.set_language(codelang)
+        self.text.set_show_line_numbers(True)
+        self.text.set_auto_indent(True)
+        self.text.set_insert_spaces_instead_of_tabs(True)
+        self.text.set_tab_width(4)
+        self.text.set_smart_home_end(True)
+        if codefont:
+            self.text.modify_font(codefont)
+        self.text.grab_focus()
+
+        # add self to notebook
+        gui.nb.append_page(self.sw, None)
+        gui.nb.set_tab_label_text(self.sw, label)
+        gui.window.show_all()
+
+    def close(self):
+        ind = self.gui.nb.page_num(self.sw)
+        self.gui.nb.remove_page(ind)
+        self.gui.pages.remove(self)
+
 class Gui:
-    def init(self):
+    def __init__(self):
         self.runner = None
         self.window = shared.gtkwin
+        self.new_page_num = 1
+        self.pages = []
 
         # table
-        self.table = gtk.Table(4, 1, False)
+        self.table = gtk.Table(3, 1, False)
         self.window.add(self.table)
 
-        # menus
+        # menu bar
         actions = gtk.ActionGroup("Actions")
         actions.add_actions(entries)
       
@@ -175,34 +231,14 @@ class Gui:
                 gtk.EXPAND | gtk.FILL, 0,
                 0,                     0)
 
-        # text edit box
-        self.sw = gtk.ScrolledWindow()
-        self.sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-        self.sw.set_shadow_type(gtk.SHADOW_IN)
-        self.table.attach(self.sw,
+        # notebook
+        self.nb = gtk.Notebook()
+        self.table.attach(self.nb,
                 # X                    # Y
                 0, 1,                  1, 2,
                 gtk.EXPAND | gtk.FILL, gtk.EXPAND | gtk.FILL,
                 0,                     0)
-
-        self.buf = gtksourceview.Buffer()
-        self.text = gtksourceview.View(self.buf)
-        self.sw.add(self.text)
-
-        # editor stuff
-        langmgr = gtksourceview.language_manager_get_default()
-        lang = langmgr.guess_language("test.py")
-        self.buf.set_language(lang)
-
-        self.text.set_show_line_numbers(True)
-        self.text.set_auto_indent(True)
-        self.text.set_insert_spaces_instead_of_tabs(True)
-        self.text.set_tab_width(4)
-        self.text.set_smart_home_end(True)
-
-        font_desc = pango.FontDescription('monospace')
-        if font_desc:
-            self.text.modify_font(font_desc)
+        self.nb.connect("focus-tab", self.focus_page)
 
         # run button
         #self.run_button = gtk.Button("Run!")
@@ -217,30 +253,66 @@ class Gui:
         self.status = gtk.Statusbar()
         self.table.attach(self.status,
                 # X                    # Y
-                0, 1,                  3, 4,
+                0, 1,                  2, 3,
                 gtk.EXPAND | gtk.FILL, 0,
                 0,                     0)
 
         # action!
+        self.add_page()
         self.window.show_all()
-        self.text.grab_focus()
+        self.nb.grab_focus()
         self.set_status("Ready!")
+
+    def add_page(self):
+        self.pages.append(Page(self, "Tab %d" % (self.new_page_num)))
+        self.set_page(-1) #last page
+        self.new_page_num += 1
+
+    def set_page(self, ind):
+        self.nb.set_current_page(ind)
+    def get_page(self):
+        return self.nb.get_current_page()
+
+    def next_page(self):
+        self.nb.next_page()
+    def prev_page(self):
+        self.nb.prev_page()
+
+    def focus_page():
+        ind = self.get_page()
+        if ind >= 0:
+            self.pages[ind].text.grab_focus()
+
+    def close_page(self):
+        ind = self.get_page()
+        if ind >= 0:
+            self.pages[ind].close()
 
     def step(self, elapsed):
         pass
 
     def run(self, *args):
         if not self.runner:
-            start, end = self.buf.get_bounds()
-            self.runner = Runner(self.buf.get_text(start, end, False))
-            self.runner.start()
-            self.set_status("Running...")
+            ind = self.get_page()
+
+            if (ind >= 0):
+                buf = self.pages[ind].buf
+                start, end = buf.get_bounds()
+                self.runner = Runner(self.pages[ind].label,
+                        buf.get_text(start, end, False))
+                self.runner.start()
+                self.set_status("Running...")
         else:
-            self.set_status("Currently running... Press Ctrl+E to Cancel.")
+            fmt = "Currently running code from '%s'... Press Ctrl+E to Cancel."
+            self.set_status(fmt % (self.runner.filename))
 
     def undo(self, *args):
-        if self.buf.can_undo():
-            self.buf.undo()
+        ind = self.get_page()
+
+        if (ind >= 0):
+            buf = self.pages[ind].buf
+            if buf.can_undo():
+                buf.undo()
 
     def set_status(self, msg):
         self.status.pop(0)
@@ -252,5 +324,7 @@ class Gui:
             self.runner = None
 
     def quit(self):
-        self.cancel()
+        if self.runner and self.runner.isAlive():
+            self.runner.kill()
+            self.runner.join()
 
